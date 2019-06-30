0850f480a955b460aa41ca3d81193f612dad9830
lz <978882523@qq.com>
2019-06-30 21:14:51 +0800

    把本地工程提交到git

---
+++ src/com/itheima/git/FirstGit.java
@@ -0,0 +1,12 @@
+package com.itheima.git;
+
+public class FirstGit {
+    public static void main(String[] args) {
+        System.out.println("git............");
+        System.out.println("git............");
+        System.out.println("git............");
+        System.out.println("git............");
+        System.out.println("git............");
+        System.out.println("git............");
+    }
+}
